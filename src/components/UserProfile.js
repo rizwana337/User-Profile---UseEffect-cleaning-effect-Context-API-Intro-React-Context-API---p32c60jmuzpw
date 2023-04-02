eaef624938ab2f3f50f8864d8de55a2ea70b9086
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-02 12:48:11 +0530

    Update UserProfile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,25 @@
-import { useContext } from 'react'
-import React from 'react'
-import { UserContext } from "./App"
+import React, { useContext } from 'react';
+import { UserContext } from './App';
 
-const UserProfile = () =>{
+const UserProfile = () => {
+  const { name, age } = useContext(UserContext);
 
-}
+  return (
+    <div>
+      <h1 id="name">Name: {name}</h1>
+      <h1 id="age">Age: {age}</h1>
+    </div>
+  );
+};
 
-export {UserProfile}
+export default UserProfile;
+
+// import { useContext } from 'react'
+// import React from 'react'
+// import { UserContext } from "./App"
+
+// const UserProfile = () =>{
+
+// }
+
+// export {UserProfile}
